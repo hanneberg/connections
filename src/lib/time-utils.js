b91751296a524fa3bc240ec0229a34a7d91318b9
Hanne Berg <hannerbe@uio.no>
2025-03-31 16:50:46 +0200

    dato

--- src/lib/time-utils.js
+++ src/lib/time-utils.js
@@ -17,7 +17,7 @@ export const getYesterday = () => startOfYesterday();
 
 // October 2023 Game Epoch
 // https://stackoverflow.com/questions/2552483/why-does-the-month-argument-range-from-0-to-11-in-javascripts-date-constructor
-export const firstGameDate = new Date(2025, 2, 27);
+export const firstGameDate = new Date(2025, 2, 31);
 export const periodInDays = 1;
 
 export const getLastGameDate = (today) => {
